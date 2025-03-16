89168d3ede6ea6913a8d2501dc0b164fa1ac9e04
yackub <yackub563@mail.ru>
2025-03-16 14:16:59 +0300

    add select queries

--- backend/embedding_system/db_crud.py
+++ backend/embedding_system/db_crud.py
@@ -1,6 +1,14 @@
+import enum
 from sqlalchemy import text
 from sqlalchemy.ext.asyncio import AsyncEngine
-from make_db import LEVEL_TABLE_NAME_PREFIX, SCHEMA_NAME
+from make_db import LEVEL_TABLE_NAME_PREFIX, SCHEMA_NAME, EMBEDDING_DIM
+
+
+class SelectIndexes(enum.Enum):
+    document_path = 0
+    document_name = 1
+    snippet = 2
+    cos_distance = 3
 
 
 class DBCrud:
@@ -11,6 +19,27 @@ class DBCrud:
             VALUES (:snippet_name, :document_path, :document_name, :snippet, :embedding)}
     """
 
+    __select_by_embedding_template = """
+        SELECT DISTINCT ON (document_path) document_path, document_name, snippet, cos_distance FROM
+            (SELECT 
+                  document_path,
+                  document_name, 
+                  snippet, 
+                  embedding <=> CAST(:query_embedding AS vector({embedding_dim})) AS cos_distance
+                FROM {table_name} ORDER BY embedding <=> CAST(:query_embedding AS vector({embedding_dim}))
+            ) distances_query 
+            ORDER BY document_path, cos_distance
+            LIMIT :limit
+    """
+
+    __select_by_name_template = """
+        SELECT DISTINCT ON (document_path)
+            document_path, :document_name AS document_name, snippet, 0 AS cos_distance 
+            FROM {table_name}
+            WHERE document_name = :document_name
+            ORDER BY document_path, snippet_name
+            LIMIT :limit
+        """
 
     def __init__(self, db_engine: AsyncEngine):
         self._db_engine: AsyncEngine = db_engine
@@ -19,7 +48,7 @@ class DBCrud:
         self._level3_name = f"{LEVEL_TABLE_NAME_PREFIX}3"
 
 
-    async def write_snippet_rows(self, list_of_rows):
+    async def write_level1_snippet_rows(self, list_of_rows):
         """
         :param list_of_rows: List[
             Dict[
@@ -32,11 +61,98 @@ class DBCrud:
         ]
         :return: None
         """
-        list_of_rows = self._unpack_snippets(document_path, document_name, snippet_bounds_batch, embeddings_batch)
-
         async with self._db_engine.begin() as connection:
             await connection.execute(
-                text(self.__insert_template.format(SCHEMA_NAME, self._level1_name)),
+                text(self.__insert_template.format(schema_name=SCHEMA_NAME, table_name=self._level1_name)),
                 list_of_rows
             )
 
+    async def write_level2_snippet_rows(self, list_of_rows):
+        """
+        :param list_of_rows: List[
+            Dict[
+                "snippet_name": str,
+                "document_path": str,
+                "document_name": str,
+                "snippet": str,
+                "embedding": List[float]
+            ]
+        ]
+        :return: None
+        """
+        async with self._db_engine.begin() as connection:
+            await connection.execute(
+                text(self.__insert_template.format(schema_name=SCHEMA_NAME, table_name=self._level2_name)),
+                list_of_rows
+            )
+
+    async def write_level3_snippet_rows(self, list_of_rows):
+        """
+        :param list_of_rows: List[
+            Dict[
+                "snippet_name": str,
+                "document_path": str,
+                "document_name": str,
+                "snippet": str,
+                "embedding": List[float]
+            ]
+        ]
+        :return: None
+        """
+        async with self._db_engine.begin() as connection:
+            await connection.execute(
+                text(self.__insert_template.format(schema_name=SCHEMA_NAME, table_name=self._level3_name)),
+                list_of_rows
+            )
+
+    async def _select_by_name(self, connection, level_table_name, document_name, limit):
+        return (
+            await connection.execute(
+                text(self.__select_by_name_template.format(table_name=level_table_name)),
+                {"document_name": document_name, "limit": limit}
+            ).all()
+        )
+
+    async def _select_from_level_for_one_embedding(self, connection, level_table_name, query_embedding_list, limit):
+        return (
+            await connection.execute(
+                text(self.__select_by_embedding_template.format(table_name=level_table_name, embedding_dim=EMBEDDING_DIM)),
+                {"limit": limit, "query_embedding": query_embedding_list[0]}
+            )
+        ).all()
+
+    async def _select_from_level(self, connection, queries_results, level_table_name, query_embedding_list, limit):
+        for i in range(len(query_embedding_list)):
+            queries_results.extend(
+                await self._select_from_level_for_one_embedding(connection, level_table_name, query_embedding_list[i], limit)
+            )
+        queries_results.sort(key=lambda x: x[SelectIndexes.cos_distance.value])
+
+        return queries_results
+
+    async def select_from_level1(self, query_embedding_list, limit):
+        with self._db_engine.begin() as connection:
+            queries_results = []
+            queries_results = self._select_from_level(
+                connection, queries_results, self._level1_name, query_embedding_list, limit
+            )
+
+        return queries_results[:limit]
+
+    async def select_from_level2(self, query_embedding_list, limit):
+        with self._db_engine.begin() as connection:
+            queries_results = []
+            queries_results = self._select_from_level(
+                connection, queries_results, self._level2_name, query_embedding_list, limit
+            )
+
+        return queries_results[:limit]
+
+    async def select_from_level3(self, query_embedding_list, limit):
+        with self._db_engine.begin() as connection:
+            queries_results = []
+            queries_results = self._select_from_level(
+                connection, queries_results, self._level3_name, query_embedding_list, limit
+            )
+
+        return queries_results[:limit]
