6631c2ad80ab91773520c9707f7b334333a2fa5e
yackub <yackub563@mail.ru>
2025-03-29 17:57:54 +0300

    update train step and train to satisfy step principe

--- backend/transformer/train_transformer.py
+++ backend/transformer/train_transformer.py
@@ -4,34 +4,39 @@ from threading import Thread
 from random import shuffle
 import torch
 from torch.utils.data import DataLoader
-
 from backend.define_torch_device import define_device
 from bidirectional_transformer import BidirectionalTransformer
 from transformers import RobertaTokenizerFast, DataCollatorForLanguageModeling
 
 
-def train_step(model, optimizer, schedule, loss_function, dataloader, batches_amount):
+def train_step(model, optimizer, schedule, loss_function, dataloader, batches_amount, step, warmup_step, total_steps):
     running_loss = 0
     for data in dataloader:
+        optimizer.zero_grad(set_to_none=True)
         x_batch = data["input_ids"]
         mask_batch = data["hugging_face_mask"]
         y_batch = data["labels"]
-        optimizer.zero_grad(set_to_none=True)
-        sample_y = model.train_forward(x_batch, hugging_face_mask=mask_batch)  # TODO unpack x
+        sample_logits = model.train_forward(x_batch, hugging_face_mask=mask_batch)  # TODO unpack x
 
-        batch, seq_len, vocab_size = sample_y.shape
-        sample_y = sample_y.view(batch * seq_len, vocab_size)
+        batch, seq_len, vocab_size = sample_logits.shape
+        sample_logits = sample_logits.view(batch * seq_len, vocab_size)
         y_batch = y_batch.view(batch * seq_len)
 
-        loss = loss_function(input=sample_y, target=y_batch)
+        loss = loss_function(input=sample_logits, target=y_batch)
         loss.backward()
         optimizer.step()
-        schedule.step()
+        step += 1
 
         running_loss += loss.detach().cpu().item()
+        end_train = (step == total_steps)
+        if end_train:
+            running_loss = running_loss / batches_amount
+            return running_loss, step, True  # loss, step, end_training
+        if step % warmup_step == 0:
+            schedule.step()
 
     running_loss = running_loss / batches_amount
-    return running_loss
+    return running_loss, step, False  # loss, step, end_training
 
 
 def validation_step(model, loss_function, dataloader, batches_amount):
@@ -40,13 +45,13 @@ def validation_step(model, loss_function, dataloader, batches_amount):
         x_batch = data["input_ids"]
         mask_batch = data["hugging_face_mask"]
         y_batch = data["labels"]
-        sample_y = model.train_forward(x_batch, hugging_face_mask=mask_batch)  # TODO unpack x
+        sample_logits = model.train_forward(x_batch, hugging_face_mask=mask_batch)  # TODO unpack x
 
-        batch, seq_len, vocab_size = sample_y.shape
-        sample_y = sample_y.view(batch * seq_len, vocab_size)
+        batch, seq_len, vocab_size = sample_logits.shape
+        sample_logits = sample_logits.view(batch * seq_len, vocab_size)
         y_batch = y_batch.view(batch * seq_len)
 
-        loss = loss_function(input=sample_y, target=y_batch)
+        loss = loss_function(input=sample_logits, target=y_batch)
 
         running_loss += loss.detach().cpu().item()
 
@@ -73,7 +78,7 @@ def save_model_daemon(model, path_to_save_models, epoch):
 def train(
     model: torch.nn.Module, optimizer: torch.optim.Optimizer, schedule, loss_function,
     train_dataloader: torch.utils.data.DataLoader, val_dataloader: torch.utils.data.DataLoader,
-    epochs_amount, save_period, path_to_save_models
+    warmup_step, total_steps, save_period, path_to_save_models
 ):
     train_start = datetime.now()
     print("Start training")
@@ -82,28 +87,30 @@ def train(
     train_batches_amount = get_batches_amount(len(train_dataloader.dataset), train_dataloader.batch_size)
     val_batches_amount = get_batches_amount(len(val_dataloader.dataset), val_dataloader.batch_size)
     model.train()
-    for epoch in range(1, epochs_amount + 1):
-        if epoch % save_period == 0:
-            print(f"Epoch {epoch}/{epochs_amount}")
-            epoch_start = datetime.now()
+    step = 0
+    while True:
+        epoch_start = datetime.now()
+        train_running_loss, step, end_training = train_step(
+            model, optimizer, schedule, loss_function, train_dataloader, train_batches_amount, step, warmup_step, total_steps
+        )
 
+        if step % save_period == 0:
+            print(f"Epoch {step + 1}/{total_steps}")
 
-        train_running_loss = train_step(model, optimizer, schedule, loss_function, train_dataloader, train_batches_amount)
-
-        if epoch % save_period == 0:
             with torch.no_grad():
                 model.eval()
                 val_running_loss = validation_step(model, loss_function, val_dataloader, val_batches_amount)
+                model.train()
 
             train_losses.append(train_running_loss)
             val_losses.append(val_running_loss)
 
-            print(f"\tEpoch is ended in {datetime.now() - epoch_start}\n\tTrain loss:\t{train_running_loss}\n\tValidation loss: {val_running_loss}")
-            save_model_daemon(model, path_to_save_models, epoch)
+            print(f"\tStep is ended in {datetime.now() - epoch_start}\n\tTrain loss:\t{train_running_loss}\n\tValidation loss: {val_running_loss}")
+            save_model_daemon(model, path_to_save_models, step)
 
-
-    print(f"Time spent on train: {datetime.now() - train_start}")
-    return train_losses, val_losses
+        if end_training:
+            print(f"Time spent on train: {datetime.now() - train_start}")
+            return train_losses, val_losses
 
 
 def save_losses(train_losses, validation_losses, filename, save_period):
@@ -144,10 +151,39 @@ def make_dataset():
 
 if __name__ == "__main__":
     tokenizer = RobertaTokenizerFast.from_pretrained("FacebookAI/roberta-large")
-    mlm = DataCollatorForLanguageModeling(tokenizer, mlm_probability=0.15, return_tensors='pt')
-
+    mlm_probability = 0.15
+    mlm = DataCollatorForLanguageModeling(tokenizer, mlm_probability=mlm_probability, return_tensors='pt')
     device = define_device()
     dtype = torch.float32
+
+    vocab_size = len(tokenizer.get_vocab())
+    max_len = 512
+    num_layers = 12
+    d_model = 768
+    num_attention_heads = 12
+    d_ffn_hidden = 3072
+    dropout_p = 0.1
+    padding_index = tokenizer.pad_token_type_id
+
+    triple_s_roberta = BidirectionalTransformer(
+        vocab_size, max_len, num_layers, d_model, num_attention_heads, d_ffn_hidden, dropout_p, device, dtype, padding_index
+    )
+
+    batch_size = 4096
+    total_steps = 1048576
+    warmup_step = 49152
+    weight_decay = 0.01
+    eps = 1e-6
+    beta1 = 0.9
+    beta2 = 0.98
+    peak_lr = 6e-4
+    optimizer = torch.optim.Adam(
+        triple_s_roberta.parameters(), lr=peak_lr, betas=(beta1, beta2), eps=eps, weight_decay=weight_decay
+    )
+    lr_decay_schedule = torch.optim.lr_scheduler.LinearLR(optimizer)
+
+    loss_function = torch.nn.CrossEntropyLoss(ignore_index=-100)
+
     
 
     text = """Meshuggah is a Swedish extreme metal band formed in Umeå in 1987. Since 2004, the band's lineup consists of founding members Jens Kidman (lead vocals) and Fredrik Thordendal (lead guitar), alongside rhythm guitarist Mårten Hagström, drummer Tomas Haake and bassist Dick Lövgren. Since its formation, the band has released nine studio albums, six EPs and eight music videos. Their latest studio album, Immutable, was released in April 2022 via Atomic Fire Records.
