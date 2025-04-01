32bc7b9b6e01e01cd30249978f554ae282652ca7
yackub <yackub563@mail.ru>
2025-04-01 17:31:09 +0300

    fix imports

--- backend/crawler.py
+++ backend/crawler.py
@@ -1,8 +1,11 @@
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler, FileDeletedEvent, FileCreatedEvent, FileModifiedEvent, FileMovedEvent
+from backend.embedding_system.embedding_system import EmbeddingSystem
 
 
 class CrawlerHandler(FileSystemEventHandler):
+    txt_postfix = ".txt"
+    pdf_postfix = ".pdf"
 
     def __init__(self):
         self.i = 0
@@ -10,37 +13,42 @@ class CrawlerHandler(FileSystemEventHandler):
         # TODO modify = delete old + create new
         # TODO move = (delete src + delete dest) + create dest
 
-    def on_created(self, event: FileCreatedEvent):
+    async def on_created(self, event: FileCreatedEvent):
         if not event.is_directory:
             print(self.i, "on_created", event.src_path, event.dest_path)
             self.i += 1
 
-    def on_deleted(self, event: FileDeletedEvent):
+    async def on_deleted(self, event: FileDeletedEvent):
         if not event.is_directory:
             print(self.i, "on_deleted", event.src_path, event.dest_path)
             self.i += 1
 
-    def on_modified(self, event: FileModifiedEvent):
+    async def on_modified(self, event: FileModifiedEvent):
         if not event.is_directory:
             print(self.i, "on_modified", event.src_path, event.dest_path)
             self.i += 1
 
-    def on_moved(self, event: FileMovedEvent):
+    async def on_moved(self, event: FileMovedEvent):
         if not event.is_directory:
             print(self.i, "on_moved", event.src_path, event.dest_path)
             self.i += 1
 
 
-handler = CrawlerHandler()
-observer = Observer()
-observer.schedule(handler, path="/home/yackub/PycharmProjects/Diploma/temp", recursive=True)
-observer.start()
+async def main():
+    handler = CrawlerHandler()
+    observer = Observer()
+    observer.schedule(handler, path="/home/yackub/PycharmProjects/Diploma/temp", recursive=True)
+    observer.start()
 
-while True:
-    try:
-        pass
-    except Exception:
-        observer.stop()
+    while True:
+        try:
+            pass
+        except Exception:
+            observer.stop()
+
+if __name__ == "__main__":
+    import asyncio
+    asyncio.run(main())
 
 
 
