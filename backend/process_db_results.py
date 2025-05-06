41b2f67e8e6ec95fea3a503a96a786025137d10e
yackub <yackub563@mail.ru>
2025-05-06 21:44:26 +0300

    add handle of whitespaces

--- backend/process_db_results.py
+++ backend/process_db_results.py
@@ -4,7 +4,7 @@ def process_db_select_results(query_results):
         results_to_show.append(
             {
                 "document_name": query_result_row.document_name,
-                "document_path": query_result_row.document_path,
+                "document_path": query_result_row.document_path.replace(" ", "%20"),
                 "snippet": query_result_row.snippet
             }
         )
