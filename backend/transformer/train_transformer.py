c1556e24069cf65e13a8e80b8a8e951f5b048342
yackub <yackub563@mail.ru>
2025-05-10 20:24:02 +0300

    .

--- backend/transformer/train_transformer.py
+++ backend/transformer/train_transformer.py
@@ -1,4 +1,5 @@
 import csv
+import json
 from copy import deepcopy
 from datetime import datetime
 from threading import Thread
@@ -93,7 +94,7 @@ def save_checkpoint_daemon(
 def train(
     model: torch.nn.Module, optimizer: torch.optim.Optimizer, schedule, loss_function,
     train_dataloader: torch.utils.data.DataLoader, val_dataloader: torch.utils.data.DataLoader,
-    warmup_step, total_steps, path_to_save_models
+    warmup_step, total_steps, path_to_save_checkpoints
 ):
     train_start = datetime.now()
     print("Start training")
@@ -126,7 +127,7 @@ def train(
         val_losses.append(val_running_loss)
 
         print(f"\tStep is ended in {datetime.now() - epoch_start}\n\tTrain loss:\t{train_running_loss}\n\tValidation loss: {val_running_loss}")
-        save_checkpoint_daemon(model, optimizer, schedule, best_model_weights, best_val_loss, step, path_to_save_models, checkpoint)
+        save_checkpoint_daemon(model, optimizer, schedule, best_model_weights, best_val_loss, step, path_to_save_checkpoints, checkpoint)
 
         if end_training:
             print(f"Time spent on train: {datetime.now() - train_start}")
@@ -135,12 +136,12 @@ def train(
         checkpoint += 1
 
 
-def save_losses(train_losses, validation_losses, filename, save_period):
+def save_losses(train_losses, validation_losses, filename):
     with open(filename, 'w') as fout:
         writer = csv.DictWriter(fout, ["Эпоха", "Ошибка обучения", "Ошибка валидации"])
         writer.writeheader()
         for i in range(len(train_losses)):
-            if i % save_period == 0:
+            if i < len(validation_losses):
                 writer.writerow(
                     {"Эпоха": i + 1, "Ошибка обучения": train_losses[i], "Ошибка валидации": validation_losses[i]}
                 )
@@ -150,37 +151,46 @@ def save_losses(train_losses, validation_losses, filename, save_period):
                 )
 
 
-def init_dataloaders(text, tokenizer, data_collator, max_length, stride, batch_size, train_part):
-    tokens_ = tokenizer(
-        text, truncation=True, padding="max_length", max_length=max_length, stride=stride,
-        return_overflowing_tokens=True, return_tensors='pt'
-    )
-    dataset = [
-        {"input_ids": tokens_["input_ids"][i], "mask": tokens_["attention_mask"][i]} for i in range(tokens.input_ids.shape[0])
-    ]
-    shuffle(dataset)
-    train_dataset = dataset[:int(len(dataset) * train_part)]
-    val_dataset = dataset[int(len(dataset) * train_part):]
+def load_dataset(path, last_index, device, ids_dtype, mask_dtype):
+    dataset = []
+    for i in range(1, last_index + 1):
+        # print(f"Reading {i}...")
+        with open(f"{path}/part{i}.json", 'r') as fin:
+            data = json.load(fin)
 
+        #print(len(data))
+        #print("Converting to torch...")
+        for j in range(len(data)):
+            data[j]["input_ids"] = torch.tensor(data[j]["input_ids"], dtype=ids_dtype, device=device,
+                                                requires_grad=False)
+            data[j]["hugging_face_mask"] = torch.tensor(data[j]["hugging_face_mask"], dtype=mask_dtype, device=device,
+                                                        requires_grad=False)
+
+        #print("Joining...")
+        dataset.extend(data)
+
+    print(f"Dataset loaded, {len(dataset)} rows")
+    return dataset
+
+
+def init_dataloaders(train_dataset, val_dataset, data_collator, batch_size):
     train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, collate_fn=data_collator)
     val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=True, collate_fn=data_collator)
     return train_loader, val_loader
 
 
-def make_dataset():
-    pass
 
 
 if __name__ == "__main__":
     tokenizer = RobertaTokenizerFast.from_pretrained("FacebookAI/roberta-large")
     mlm_probability = 0.15
-    mlm = DataCollatorForLanguageModeling(tokenizer, mlm_probability=mlm_probability, return_tensors='pt')
-    device = define_device()
-    dtype = torch.float32
+    mlm_collator = DataCollatorForLanguageModeling(tokenizer, mlm_probability=mlm_probability, return_tensors='pt')
+    model_device = define_device()
+    model_dtype = torch.float32
 
     vocab_size = len(tokenizer.get_vocab())
     max_len = 256
-    stride = 0
+    #stride = 0
     num_layers = 12
     d_model = 768
     num_attention_heads = 12
@@ -189,7 +199,7 @@ if __name__ == "__main__":
     padding_index = tokenizer.pad_token_type_id
 
     triple_s_roberta = BidirectionalTransformer(
-        vocab_size, max_len, num_layers, d_model, num_attention_heads, d_ffn_hidden, dropout_p, device, dtype, padding_index
+        vocab_size, max_len, num_layers, d_model, num_attention_heads, d_ffn_hidden, dropout_p, model_device, model_dtype, padding_index
     )
 
     batch_size = 4096
@@ -207,38 +217,84 @@ if __name__ == "__main__":
 
     loss_function = torch.nn.CrossEntropyLoss(ignore_index=-100)
 
-    
+    path_to_save_checkpoints = "C:/Users/amis-/PycharmProjects/semantic_search_system/backend/transformer/train_logs/1/checkpoints"
+    losses_filename = "C:/Users/amis-/PycharmProjects/semantic_search_system/backend/transformer/train_logs/1/losses.csv"
 
-    text = """Meshuggah is a Swedish extreme metal band formed in Umeå in 1987. Since 2004, the band's lineup consists of founding members Jens Kidman (lead vocals) and Fredrik Thordendal (lead guitar), alongside rhythm guitarist Mårten Hagström, drummer Tomas Haake and bassist Dick Lövgren. Since its formation, the band has released nine studio albums, six EPs and eight music videos. Their latest studio album, Immutable, was released in April 2022 via Atomic Fire Records.
-        Meshuggah has become known for their innovative musical style and their complex, polymetered song structures and polyrhythms. They rose to fame as a significant act in extreme underground music, became an influence for modern metal bands, and gained a cult following. The band was labelled as one of the ten most important hard rock and heavy metal bands by Rolling Stone and as the most important band in metal by Alternative Press. In the late 2000s, the band was an inspiration for the djent subgenre.
-        In 2006 and 2009, Meshuggah was nominated for two Swedish Grammis Awards for their albums Catch Thirtythree and obZen, respectively. In 2018, the band was nominated for a Grammy Award for their song "Clockworks" under the "Best Metal Performance" category.[2] The band has performed in various international festivals, including Ozzfest and Download, and embarked on the obZen world tour from 2008 to 2010, and also the "Ophidian Trek".
-        """
-    from pprint import pprint
-    tokens = tokenizer(text, truncation=True, padding="max_length", max_length=8, stride=4, return_tensors='pt', return_overflowing_tokens=True)
-    print(tokens.input_ids.shape)
+    data_device = torch.device("cpu")
+    ids_dtype = torch.uint16
+    mask_dtype = torch.int8
 
-    dataset = [
-        {"input_ids": tokens["input_ids"][i], "hugging_face_mask": tokens["attention_mask"][i]} for i in range(tokens.input_ids.shape[0])
-    ]
-    #pprint(dataset)
-    #print(dataset)
+    train_dataset_path = "C:/Users/amis-/PycharmProjects/semantic_search_system/backend/new_datasets/train"
+    train_last_index = 43
 
+    val_dataset_path = "C:/Users/amis-/PycharmProjects/semantic_search_system/backend/new_datasets/val"
+    val_last_index = 9
 
-    model = BidirectionalTransformer(len(tokenizer.get_vocab()), 8, 1, 32, 2, 14, 0, torch.device("cuda"), torch.float32, tokenizer.pad_token_type_id)
+    train_dataset = load_dataset(train_dataset_path, train_last_index, data_device, ids_dtype, mask_dtype)
+    val_dataset = load_dataset(val_dataset_path, val_last_index, data_device, ids_dtype, mask_dtype)
 
+    train_dataloader, val_dataloader = init_dataloaders(train_dataset, val_dataset, mlm_collator, batch_size)
 
+    train_losses, val_losses, best_model_weights = train(
+        triple_s_roberta, optimizer, lr_decay_schedule, loss_function,
+        train_dataloader, val_dataloader,
+        warmup_step, total_steps,
+        path_to_save_checkpoints
+    )
 
-    from bidirectional_transformer import make_mask
-    from torch.utils.data import DataLoader
+    save_losses_thread = Thread(target=save_losses, args=(train_losses, val_losses, losses_filename))
+    save_losses_thread.start()
 
-    loader = DataLoader(dataset, batch_size=2, shuffle=False, collate_fn=mlm)
-    for i, x in enumerate(loader):
-        print(i, x)
-        break
+    checkpoint = {
+        "best_weights": best_model_weights,
+        "optimizer": deepcopy(optimizer.state_dict()),
+        "lr_decay_schedule": deepcopy(lr_decay_schedule.state_dict())
+    }
+    save_model_thread = Thread(
+        target=torch.save,
+        args=(
+            checkpoint,
+            f"{path_to_save_checkpoints}/checkpoint_after_training.pth"
+        ),
+        daemon=False
+    )
+    save_model_thread.start()
 
-    for i, x in enumerate(loader):
-        print(i, x)
-        break
+    save_losses_thread.join()
+    save_model_thread.join()
+
+    #
+    #
+    # text = """Meshuggah is a Swedish extreme metal band formed in Umeå in 1987. Since 2004, the band's lineup consists of founding members Jens Kidman (lead vocals) and Fredrik Thordendal (lead guitar), alongside rhythm guitarist Mårten Hagström, drummer Tomas Haake and bassist Dick Lövgren. Since its formation, the band has released nine studio albums, six EPs and eight music videos. Their latest studio album, Immutable, was released in April 2022 via Atomic Fire Records.
+    #     Meshuggah has become known for their innovative musical style and their complex, polymetered song structures and polyrhythms. They rose to fame as a significant act in extreme underground music, became an influence for modern metal bands, and gained a cult following. The band was labelled as one of the ten most important hard rock and heavy metal bands by Rolling Stone and as the most important band in metal by Alternative Press. In the late 2000s, the band was an inspiration for the djent subgenre.
+    #     In 2006 and 2009, Meshuggah was nominated for two Swedish Grammis Awards for their albums Catch Thirtythree and obZen, respectively. In 2018, the band was nominated for a Grammy Award for their song "Clockworks" under the "Best Metal Performance" category.[2] The band has performed in various international festivals, including Ozzfest and Download, and embarked on the obZen world tour from 2008 to 2010, and also the "Ophidian Trek".
+    #     """
+    # from pprint import pprint
+    # tokens = tokenizer(text, truncation=True, padding="max_length", max_length=8, stride=4, return_tensors='pt', return_overflowing_tokens=True)
+    # print(tokens.input_ids.shape)
+    #
+    # dataset = [
+    #     {"input_ids": tokens["input_ids"][i], "hugging_face_mask": tokens["attention_mask"][i]} for i in range(tokens.input_ids.shape[0])
+    # ]
+    # #pprint(dataset)
+    # #print(dataset)
+    #
+    #
+    # model = BidirectionalTransformer(len(tokenizer.get_vocab()), 8, 1, 32, 2, 14, 0, torch.device("cuda"), torch.float32, tokenizer.pad_token_type_id)
+    #
+    #
+    #
+    # from bidirectional_transformer import make_mask
+    # from torch.utils.data import DataLoader
+    #
+    # loader = DataLoader(dataset, batch_size=2, shuffle=False, collate_fn=mlm_collator)
+    # for i, x in enumerate(loader):
+    #     print(i, x)
+    #     break
+    #
+    # for i, x in enumerate(loader):
+    #     print(i, x)
+    #     break
 
     # for x in loader:
     #     #print(make_mask(x["hugging_face_mask"], torch.float32).shape)
